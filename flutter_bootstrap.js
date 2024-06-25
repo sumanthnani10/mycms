a3426955d6752ea669a7dd605dccdcccbceca07b
sumanthnani10 <sumanthnani10@gmail.com>
2024-06-24 23:47:59 -0400

    clipboard

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "680543943"
+    serviceWorkerVersion: "1585334681"
   }
 });
